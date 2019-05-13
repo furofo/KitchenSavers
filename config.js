0ddaed70769be4bfc01a27f81dc3e23ed749bf06
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-13 00:07:02 -0400

    added changing background images

--- config.js
+++ config.js
@@ -11,4 +11,23 @@ window.addEventListener("DOMContentLoaded", function() {
   document.getElementById("gallerybtn").onclick = function() {
     location.href = "/gallery.html";
   };
+
+  var header = document.getElementById("indexbackground1");
+
+  var backgrounds = new Array(
+    "url(img/background1.jpeg)",
+    "url(img/background2.jpeg)"
+  );
+
+  var current = 0;
+
+  function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    header.style.backgroundImage = backgrounds[current];
+    
+  }
+  setInterval(nextBackground, 1000);
+
+  header.style.backgroundImage = backgrounds[0];
 });
