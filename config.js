b818d84f528e0826a2829f2ba2c3bb4377fda1e4
Christian Malpass <chriscellphonerepair@gmail.com>
2019-05-24 19:54:24 -0400

    test to fix git hub pages links

--- config.js
+++ config.js
@@ -36,6 +36,13 @@ window.addEventListener("DOMContentLoaded", function() {
   // backgroundimg.style.background = backgrounds[0];
 
   /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
   
 });
 
